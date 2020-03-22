887f7a78af0b8d628aef5e8c9b3eb330d075a836
lilxdata <madeline_pignetti@hmc.edu>
2020-03-22 16:07:36 -0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by madi on 3/22/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
