f1e2a487c3c378727adeb20638f2ff4c3963ed5b
lilxdata <madeline_pignetti@hmc.edu>
2020-03-22 17:54:27 -0700

    add @IBOutlet and @IBActions, update score and draw cards

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,10 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var CPUScoreLabel: UILabel!
+    
+    
+    
+    var playerScore = 0
+    var CPUScore = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+              
+        //randomly draw player card
+        leftImageView.image = UIImage(named:"\(leftNumber)")
+        
+        //randomly draw cpu card
+        rightImageView.image = UIImage(named: "\(rightNumber)")
+        
+        //see who wins and give them points
+        if leftNumber > rightNumber {
+            playerScore += 1
+            playerScoreLabel.text = "\(playerScore)"
+        } else if rightNumber > leftNumber {
+            CPUScore += 1
+            CPUScoreLabel.text = "\(CPUScore)"
+        } else {
+            playerScore += 1
+            CPUScore += 1
+            playerScoreLabel.text = "\(playerScore)"
+            CPUScoreLabel.text = "\(CPUScore)"
+        }
+      
+    }
+    
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        
+        //set cards back to default card back
+        leftImageView.image = UIImage(named:"card_back")
+        
+        rightImageView.image = UIImage(named: "card_back")
+        
+        //set scores back to 0
+        playerScore = 0
+        CPUScore = 0
+        playerScoreLabel.text = "\(playerScore)"
+        CPUScoreLabel.text = "\(CPUScore)"
+        
+    }
+    
 
 
 }
